8a2d9ffc8137a5d2efa7e0e3f2e21766131df38f
Anurag Rawat <rawatanurag180@gmail.com>
2025-08-19 23:50:33 +0530

    Minor changes

--- socialbuzz/settings.py
+++ socialbuzz/settings.py
@@ -126,6 +126,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+# WhiteNoise storage backend: compressed + hashed filenames
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
