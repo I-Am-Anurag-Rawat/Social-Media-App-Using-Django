956037db21ff14b71a1ba2e8b5aff4c0696958ee
Anurag Rawat <rawatanurag180@gmail.com>
2025-08-19 23:29:58 +0530

    Minor changes

--- socialbuzz/settings.py
+++ socialbuzz/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "False"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 print(DEBUG)
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
